411a876c96d024abdc4b4a83f22ca634812dd686
dvstefanov <d.stefanov088@gmail.com>
2024-03-29 19:36:20 +0200

    Initial upload of project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<header>
+				<h1>Tic Tac Toe</h1>
+			</header>
+			<div id='game-board-wrapper'>
+				<div id='players-wrapper'>
+					<div>Player X</div>
+					<div>Player O</div>
+				</div>
+			</div>
+			<div>Logs</div>
+		</>
+	);
+}
+
+export default App;
