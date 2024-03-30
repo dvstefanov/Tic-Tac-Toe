8307db7f6e279a2ac699dba919ca899cb26152bf
dvstefanov <d.stefanov088@gmail.com>
2024-03-30 16:00:23 +0200

    Update readme

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -3,7 +3,7 @@ import classes from './GameOver.module.css';
 const GameOver = ({ winner, onReset }) => {
 	return (
 		<div className={classes.wrapper}>
-			{winner !== undefined ? <p className={classes.text}>{winner} won!!!</p> : <p className={classes.text}>It's a draw!</p>}
+			{winner !== undefined ? <p className={classes.text}>{winner} won!</p> : <p className={classes.text}>It's a draw!</p>}
 			<button onClick={onReset} className={classes.btn}>Rematch</button>
 		</div>
 	);
